4d92afa71a193ee2d8cd3894fd99e5c0249fbc43
Edgar Mkhitaryan <edgar.mkhitaryan1989@gmail.com>
2022-11-14 15:53:33 +0400

    Encopsulation, create models

---
+++ sic-backend/models/User.model.ts
@@ -0,0 +1,19 @@
+export type USER_RESPONSE = {
+    first_name: string,
+    surname: string,
+    email: string,
+    created_on: string,
+    last_login: string,
+    public_id: string
+}
+
+export type USER = {
+    id: number,
+    first_name: string,
+    surname: string,
+    password: string,
+    email: string,
+    created_on: string,
+    last_login: string,
+    public_id: string
+}
---
+++ sic-backend/utilities/requests/userRequests.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { USER, USER_RESPONSE } from '../../models/User.model';
+const pool = require('../../db')
+
+export const getUserByEmail = async (req: Request, res: Response) => {
+    try {
+        const body = req.body
+        const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [body.email]);
+        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
+        return res.status(200).send(userResponse)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const getUserByPublicId = async (req: Request, res: Response) => {
+    try {
+        const { id: userId } = req.params
+        const { rows } = await pool.query('SELECT * FROM users WHERE public_id = $1', [userId]);
+        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
+        return res.status(200).send(userResponse)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const createNewUser = async (req: Request, res: Response) => {
+    try {
+        const body = req.body;
+        const { rows } = await pool.query(
+            "INSERT INTO users (first_name, surname, password, email) VALUES ($1,$2,$3,$4) RETURNING *",
+            [body.first_name, body.surname, body.password, body.email]
+        );
+        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
+        return res.status(200).send(userResponse)
+    } catch (error) {
+        console.error(error);
+    }
+}
