18371297fbe956eef9ccc7af642e16839753a5bf
Edgar Mkhitaryan <edgar.mkhitaryan1989@gmail.com>
2022-11-30 19:30:17 +0400

    passport tmp configs

--- sic-backend/routes/auth.ts
+++ sic-backend/routes/auth.ts
@@ -3,23 +3,61 @@ import { v4 as uuidv4 } from 'uuid';
 import { USER } from "../models/User.model";
 const Router = require('express-promise-router')
 const session = require('express-session')
-const pool = require('../db')
+// const pool = require('../db')
 const pgSession = require('connect-pg-simple')(session)
 const passport = require('passport');
 
+require("dotenv").config();
+
+const { Pool } = require("pg");
+// const pool = new Pool({
+//   user: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432,
+// });
+
+const conObject = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sic_backend',
+  password: 'Dwin_Tech',
+  port: 5432,
+}
+
+const pgStoreConfig = {
+  // pgPromise: require('pg-promise')({ promiseLib: require('bluebird') })({ conObject }),
+  conString:'postgres://postgres:Dwin_Tech@localhost:5432/sic_backend"'
+}
+
+// var PostgreSqlStore = require('connect-pg-simple')(session);
+// var sessionOptions = {
+//       secret: "secret",
+//       resave : true,
+//       saveUninitialized : false,
+//       store : new PostgreSqlStore({
+//         /*
+//         connection string is built by following the syntax:
+//         postgres://USERNAME:PASSWORD@HOST_NAME:PORT/DB_NAME
+//         */
+//         conString: "postgres://postgres:postgres@localhost:5433/postgres"
+//       })
+//     };
+
+//     app.use(session(sessionOptions));
+
 const router = new Router()
 router.use(session({
-  store: new pgSession({
-    pool: pool,
-    tableName: 'session'
-    // Insert connect-pg-simple options here
-  }),
+  store: new pgSession(pgStoreConfig),
   secret: process.env.COOKIE_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
   // Insert express-session options here
 }));
+
+postgres://postgres:postgres@localhost:5433/postgres
 
 
 require('../utilities/passport.auth');
