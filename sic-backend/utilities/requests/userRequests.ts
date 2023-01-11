b5f4b60ec11506abff837df4d5ffa8dcdbc9c8e4
Edgar Mkhitaryan <edgar.mkhitaryan1989@gmail.com>
2023-01-11 11:07:18 +0400

    Add error handling to user create

--- sic-backend/utilities/requests/userRequests.ts
+++ sic-backend/utilities/requests/userRequests.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from 'express';
-import { USER, USER_RESPONSE } from '../../models/User.model';
+import createHttpError from 'http-errors';
+import { USER_RESPONSE } from '../../models/User.model';
 const pool = require('../../db')
 
 export const getUserByEmailClient = async (req: Request, res: Response) => {
     try {
         const body = req.body
         const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [body.email]);
-        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const { first_name, surname, email, created_on, last_login, public_id } = rows[0]
         const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
         return res.status(200).send(userResponse)
     } catch (error) {
         console.error(error)
+        throw createHttpError(400, { expose: false, message: error })
     }
 }
 
@@ -18,11 +20,12 @@ export const getUserByPublicIdClient = async (req: Request, res: Response) => {
     try {
         const { id: userId } = req.params
         const { rows } = await pool.query('SELECT * FROM users WHERE public_id = $1', [userId]);
-        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const { first_name, surname, email, created_on, last_login, public_id } = rows[0]
         const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
         return res.status(200).send(userResponse)
     } catch (error) {
         console.error(error)
+        throw createHttpError(400, { expose: false, message: error })
     }
 }
 
@@ -33,10 +36,11 @@ export const createNewUser = async (req: Request, res: Response) => {
             "INSERT INTO users (first_name, surname, password, email) VALUES ($1,$2,$3,$4) RETURNING *",
             [body.first_name, body.surname, body.password, body.email]
         );
-        const { first_name, surname, email, created_on, last_login, public_id, ...rest } = rows[0]
+        const { first_name, surname, email, created_on, last_login, public_id } = rows[0]
         const userResponse: USER_RESPONSE = { first_name, surname, email, created_on, last_login, public_id };
-         res.status(200).send(userResponse).redirect('auth/login');
+        res.status(200).send(userResponse).redirect('auth/login');
     } catch (error) {
         console.error(error);
+        throw createHttpError(400, { expose: false, message: error })
     }
 }
